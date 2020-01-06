9ad129a81918f7cf3609c0accbacfbd3625a825c
maruware <me@maruware.com>
2020-01-06 15:24:21 +0900

    support codecov

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/']
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  coverageDirectory: "./coverage/"
 }
